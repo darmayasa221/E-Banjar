bdfe8a4b4a6088abb9f2304cfdf112a51e23d68f
darmayasa221 <darmayasa221@gmail.com>
2021-07-30 16:05:02 +0800

    add tailwind ccs

--- index.php
+++ index.php
@@ -7,9 +7,28 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/navbar.css">
+  <link rel="stylesheet" href="assets/css/tailwind.css">
   <title>E-Banjar</title>
 </head>
 <body>
-  
+  <nav class="navbar_container">
+    <div class="navbar_content">
+      <a href="#">
+        Login
+      </a>
+    </div>
+  </nav>
+  <main>
+    <div class="search_container">
+      <div class="search_content ">
+        <h1>Wellcome To E-Banjar</h1>
+        <h2>Search Any What You Want...</h2>
+        <form>
+          <Input class="h-14 w-96 rounded z-0 focus:shadow focus:outline-none"/>
+        </form>
+      </div>
+    </div>
+  </main>
 </body>
 </html>
